85bc7e3dfabd6e3b3312f87a886fcabc7bd96c7c
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-02 17:49:02 +0530

    Update Yukki.py

--- KIGO/Core/PyTgCalls/Yukki.py
+++ KIGO/Core/PyTgCalls/Yukki.py
@@ -18,21 +18,21 @@ from pytgcalls.types.input_stream.quality import (HighQualityAudio,
 from pytgcalls.types.stream import StreamAudioEnded, StreamVideoEnded
 
 from config import STRING1, STRING2, STRING3, STRING4, STRING5, get_queue
-from Yukki import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5,
+from KIGO import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5,
                    MUSIC_BOT_NAME, app, db_mem)
-from Yukki.Core.PyTgCalls import Queues
-from Yukki.Core.PyTgCalls.Converter import convert
-from Yukki.Core.PyTgCalls.Downloader import download
-from Yukki.Database import (get_assistant, remove_active_chat,
+from KIGO.Core.PyTgCalls import Queues
+from KIGOW.Core.PyTgCalls.Converter import convert
+from KIGO.Core.PyTgCalls.Downloader import download
+from KIGO.Database import (get_assistant, remove_active_chat,
                             remove_active_video_chat)
-from Yukki.Inline import (audio_markup, audio_timer_markup_start,
+from KIGO.Inline import (audio_markup, audio_timer_markup_start,
                           primary_markup, secondary_markup2, timer_markup)
-from Yukki.Utilities.changers import time_to_seconds
-from Yukki.Utilities.chat import specialfont_to_normal
-from Yukki.Utilities.theme import check_theme
-from Yukki.Utilities.thumbnails import gen_thumb
-from Yukki.Utilities.timer import start_timer
-from Yukki.Utilities.youtube import get_m3u8, get_yt_info_id
+from KIGO.Utilities.changers import time_to_seconds
+from KIGO.Utilities.chat import specialfont_to_normal
+from KIGO.Utilities.theme import check_theme
+from KIGO.Utilities.thumbnails import gen_thumb
+from KIGO.Utilities.timer import start_timer
+from KIGO.Utilities.youtube import get_m3u8, get_yt_info_id
 
 ### Clients
 pytgcalls1 = PyTgCalls(ASS_CLI_1)
