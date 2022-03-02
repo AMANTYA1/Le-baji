e665faf8604d059849d1ba085a2e37f0d081398d
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-02 18:29:21 +0530

    Update admins.py

--- KIGO/Decorators/admins.py
+++ KIGO/Decorators/admins.py
@@ -1,11 +1,11 @@
 from typing import Dict, List, Union
 
-from Yukki import SUDOERS, app
-from Yukki.Database import (_get_authusers, add_nonadmin_chat, delete_authuser,
+from KIGO import SUDOERS, app
+from KIGO.Database import (_get_authusers, add_nonadmin_chat, delete_authuser,
                             get_authuser, get_authuser_count,
                             get_authuser_names, is_nonadmin_chat,
                             remove_nonadmin_chat, save_authuser)
-from Yukki.Utilities.changers import int_to_alpha
+from KIGO.Utilities.changers import int_to_alpha
 
 
 def AdminRightsCheck(mystic):
