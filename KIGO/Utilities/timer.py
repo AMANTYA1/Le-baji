c356d374f067388758f6b1f44e28e9d974afe854
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-02 15:54:53 +0530

    Update timer.py

--- KIGO/Utilities/timer.py
+++ KIGO/Utilities/timer.py
@@ -6,9 +6,9 @@ from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, InputMediaPhoto, Message)
 
-from Yukki import db_mem
-from Yukki.Database import is_active_chat, is_music_playing
-from Yukki.Inline import audio_timer_markup_start, timer_markup
+from KIGO import db_mem
+from KIGO.Database import is_active_chat, is_music_playing
+from KIGO.Inline import audio_timer_markup_start, timer_markup
 
 
 async def start_timer(
