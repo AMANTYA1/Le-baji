423140ecb4a550da223408a290c8b43974fdfdd0
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-02 20:19:00 +0530

    Update others.py

--- KIGO/Inline/others.py
+++ KIGO/Inline/others.py
@@ -1,7 +1,7 @@
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, InputMediaPhoto, Message)
 
-from Yukki import db_mem
+from KIGO import db_mem
 
 
 def others_markup(videoid, user_id):
