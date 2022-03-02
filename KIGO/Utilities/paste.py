f79ff589529faa7c12bb628c047fc3cd823a7829
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-02 15:46:19 +0530

    Update paste.py

--- KIGO/Utilities/paste.py
+++ KIGO/Utilities/paste.py
@@ -3,7 +3,7 @@ import socket
 from asyncio import get_running_loop
 from functools import partial
 
-from Yukki import aiohttpsession as session
+from KIGO import aiohttpsession as session
 
 
 def _netcat(host, port, content):
