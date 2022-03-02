8ae7613834843b1b74746d21c0ee65d65ecc7efc
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-02 19:00:55 +0530

    Update Playlist.py

--- KIGO/Plugins/Playlist.py
+++ KIGO/Plugins/Playlist.py
@@ -3,14 +3,14 @@ from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
                             KeyboardButton, Message, ReplyKeyboardMarkup,
                             ReplyKeyboardRemove)
 
-from Yukki import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
-from Yukki.Database import (_get_playlists, delete_playlist, get_playlist,
+from KIGO import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
+from KIGO.Database import (_get_playlists, delete_playlist, get_playlist,
                             get_playlist_names, save_playlist)
-from Yukki.Decorators.admins import AdminRightsCheck
-from Yukki.Decorators.assistant import AssistantAdd
-from Yukki.Decorators.checker import checker, checkerCB
-from Yukki.Decorators.permission import PermissionCheck
-from Yukki.Inline import (add_genre_markup, check_genre_markup, check_markup,
+from KIGO.Decorators.admins import AdminRightsCheck
+from KIGO.Decorators.assistant import AssistantAdd
+from KIGO.Decorators.checker import checker, checkerCB
+from KIGO.Decorators.permission import PermissionCheck
+from KIGO.Inline import (add_genre_markup, check_genre_markup, check_markup,
                           delete_playlist_markuup, download_markup,
                           others_markup, play_genre_playlist, playlist_markup,
                           third_playlist_markup)
@@ -33,6 +33,8 @@ __HELP__ = """
 
 /delgroupplaylist
 - Delete any saved music in your group's playlist [Requires Admin Rights.]
+
+**✗  Pᴏᴡᴇʀᴇᴅ 🔥 Bʏ: Kɪɢᴏ Dᴜɴɪʏᴀ!**
 """
 
 
