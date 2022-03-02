0696d4518b59044c514d71d7c7cd670ea59f053d
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-02 20:51:59 +0530

    Update Stats.py

--- KIGO/Plugins/Stats.py
+++ KIGO/Plugins/Stats.py
@@ -183,7 +183,7 @@ async def stats_markup(_, CallbackQuery):
                     "Failed to get Mongo DB stats", reply_markup=stats5
                 )
             try:
-                db = pymongo.Yukki
+                db = pymongo.KIGO
             except Exception as e:
                 print(e)
                 return await CallbackQuery.edit_message_text(
