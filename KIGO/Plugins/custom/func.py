2f60a8e94fb5c5e22246aefae75badca76950ad8
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-02 20:45:31 +0530

    Update func.py

--- KIGO/Plugins/custom/func.py
+++ KIGO/Plugins/custom/func.py
@@ -39,7 +39,7 @@ from KIGO.Utilities.youtube import get_m3u8
 from config import get_queue
 from KIGO import BOT_USERNAME, db_mem
 from KIGO.Core.PyTgCalls import Queues
-from KIGO.Core.PyTgCalls.Yukki import (join_live_stream, join_video_stream,
+from KIGO.Core.PyTgCalls.KIGO import (join_live_stream, join_video_stream,
                                         stop_stream)
 from KIGO.Database import (add_active_chat, add_active_video_chat,
                             is_active_chat, music_off, music_on,
@@ -47,7 +47,7 @@ from KIGO.Database import (add_active_chat, add_active_video_chat,
 from KIGO.Inline import (audio_markup, audio_markup2, primary_markup,
                           secondary_markup, secondary_markup2)
 from KIGO.Utilities.timer import start_timer
-from KIGO.Core.PyTgCalls.Yukki import join_stream
+from KIGO.Core.PyTgCalls.KIGO import join_stream
 from KIGO.Database import (add_active_chat, add_active_video_chat,
                             is_active_chat, music_off, music_on)
 from KIGO.Inline import (audio_markup, audio_markup2, primary_markup,
